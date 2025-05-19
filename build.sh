0bb56e159b99dde474c54ac8448352f02709a86a
marc-dantas <marciodantasms@hotmail.com>
2025-05-19 17:13:20 -0300

    added compilation options for windows

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+
 CFLAGS="-Wall -Wextra"
 MAIN="src/main.c"
-INCLUDE="/usr/share/include"
+INCLUDE="/usr/local/include"
+# OUT="bin/inkpad.exe"
 OUT="bin/inkpad"
 set -xe
 mkdir -p bin
-gcc $MAIN -o $OUT -I$INCLUDE $CFLAGS -lraylib -lm
+
+# x86_64-w64-mingw32-gcc-win32 $MAIN -o $OUT -I$INCLUDE $CFLAGS -L./lib -lraylib -lwinmm -lgdi32
+cc $MAIN -o $OUT -I$INCLUDE $CFLAGS -lraylib -lm
